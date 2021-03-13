c8280c73c051dd24d73bb3abfece868edb4598ca
İbrahim Ersin YAVAŞ <ersin.yavas@etiya.com>
2021-03-13 13:50:11 +0300

    Entityler oluşturuldu

--- api/src/main/java/com/linktera/linkteraquiz/model/Author.java
+++ api/src/main/java/com/linktera/linkteraquiz/model/Author.java
@@ -3,22 +3,17 @@ package com.linktera.linkteraquiz.model;
 import lombok.Data;
 
 import javax.persistence.*;
-import java.util.UUID;
 
 @Data
 @Entity
-@Table(name = "book")
-public class Book {
+@Table(name = "author")
+public class Author {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "title")
-    private String title;
-
-    @Column(name = "author")
-    private String author;
-
+    @Column(name = "name")
+    private String name;
 }
